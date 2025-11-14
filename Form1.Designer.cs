6bc689ecb5a08c3e90a3573828c6d0e12c5d9293
dn192246 <ricardo@mail.com>
2025-11-14 12:28:01 -0600

    Casi terminado

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTijera = new System.Windows.Forms.Button();
+            this.btnPapel = new System.Windows.Forms.Button();
+            this.btnPiedra = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.lbMaquina = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Piedra, Papel o Tijeras";
+            // 
+            // btnTijera
+            // 
+            this.btnTijera.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTijera.BackgroundImage")));
+            this.btnTijera.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTijera.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTijera.Location = new System.Drawing.Point(33, 202);
+            this.btnTijera.Name = "btnTijera";
+            this.btnTijera.Size = new System.Drawing.Size(105, 88);
+            this.btnTijera.TabIndex = 1;
+            this.btnTijera.UseVisualStyleBackColor = true;
+            this.btnTijera.Click += new System.EventHandler(this.btnTijera_Click);
+            // 
+            // btnPapel
+            // 
+            this.btnPapel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPapel.BackgroundImage")));
+            this.btnPapel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPapel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPapel.Location = new System.Drawing.Point(255, 202);
+            this.btnPapel.Name = "btnPapel";
+            this.btnPapel.Size = new System.Drawing.Size(105, 88);
+            this.btnPapel.TabIndex = 2;
+            this.btnPapel.UseVisualStyleBackColor = true;
+            this.btnPapel.Click += new System.EventHandler(this.btnPapel_Click);
+            // 
+            // btnPiedra
+            // 
+            this.btnPiedra.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPiedra.BackgroundImage")));
+            this.btnPiedra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPiedra.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPiedra.Location = new System.Drawing.Point(144, 202);
+            this.btnPiedra.Name = "btnPiedra";
+            this.btnPiedra.Size = new System.Drawing.Size(105, 88);
+            this.btnPiedra.TabIndex = 3;
+            this.btnPiedra.UseVisualStyleBackColor = true;
+            this.btnPiedra.Click += new System.EventHandler(this.btnPiedra_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(141, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Jugador";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(540, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Máquina";
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado.Location = new System.Drawing.Point(341, 318);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(88, 21);
+            this.lbResultado.TabIndex = 6;
+            this.lbResultado.Text = "Resultado";
+            // 
+            // lbMaquina
+            // 
+            this.lbMaquina.AutoSize = true;
+            this.lbMaquina.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMaquina.Location = new System.Drawing.Point(540, 217);
+            this.lbMaquina.Name = "lbMaquina";
+            this.lbMaquina.Size = new System.Drawing.Size(17, 21);
+            this.lbMaquina.TabIndex = 7;
+            this.lbMaquina.Text = "/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(449, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Escoge una opción y espera a que la máquina responda";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(744, 374);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbMaquina);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPiedra);
+            this.Controls.Add(this.btnPapel);
+            this.Controls.Add(this.btnTijera);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTijera;
+        private System.Windows.Forms.Button btnPapel;
+        private System.Windows.Forms.Button btnPiedra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Label lbMaquina;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Jugar(string usuario)
+        {
+            Random aleatorio = new Random();
+            int numero = aleatorio.Next(1, 3);
+            string maquina = "";
+
+            if (numero == 1) 
+            {
+                maquina = "Piedra";
+                lbMaquina.Text = "Piedra";
+            }
+            
+            if (numero == 2)
+            {
+                maquina = "Papel"; 
+                lbMaquina.Text = "Papel";
+            }
+
+            if (numero == 3) 
+            {
+                maquina = "Tijeras";
+                lbMaquina.Text = "Tijeras";
+            } 
+
+            if (usuario == maquina)
+            {
+                lbResultado.Text = "Empate";
+            }
+
+            else if (usuario == "Piedra" && maquina == "Tijeras" ||
+                     usuario == "Papel" && maquina == "Piedra" ||
+                     usuario == "Tijeras" && maquina == "Papel")
+            {
+                lbResultado.Text = "Ganaste";
+            }
+
+            else
+            {
+                lbResultado.Text = "Perdiste";
+            }
+        }
+
+        private void btnTijera_Click(object sender, EventArgs e)
+        {
+            Jugar("Tijeras");
+        }
+
+        private void btnPiedra_Click(object sender, EventArgs e)
+        {
+            Jugar("Piedra");
+        }
+
+        private void btnPapel_Click(object sender, EventArgs e)
+        {
+            Jugar("Papel");
+        }
+    }
+}
